0d6807212e536cc9854fdec0c7d1046ea75255c9
N003AD <diop97tidiane@gmail.com>
2024-05-16 11:23:41 +0000

    First Commit

--- index.js
+++ index.js
@@ -1,25 +1,9 @@
 const express = require('express')
 var bodyParser = require('body-parser')
-
-
-const path = require('path')
 const app = express()
 
-
-app.use(express.static(path.join(__dirname, 'static')))
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
-
-// Pour recupérer des informations dans le params on mets :filname
-app.get('/static/:filename', function (req, res) {
-    console.log("Valeur du params", req.params)
-    console.log("Valeur du query", req.query)
-    console.log("Valeur du body", req.body)
-    console.log("Valeur du headers", req.headers)
-
-// res.sendFile(path.join(__dirname, "static", "login.html"));
-res.sendFile(path.join(__dirname, "static", req.params.filename));
-})
 
 app.get('/', function (req, res) {
     contenuHtml = `
